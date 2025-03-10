8932a89230e4411ff9888524253c44b02597b5e6
Fran-mndz <shiro54828@gmail.com>
2025-03-10 01:30:12 -0600

    Creacion de las clases y DbContext y modificacion del appSettings

---
+++ TAREA_MODULO.AppMVCCodeFirst/Models/TareaDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TAREA_MODULO.AppMVCCodeFirst.Models
+{
+    public class TareaDbContext: DbContext
+    {
+        public TareaDbContext(DbContextOptions<TareaDbContext> options) : base(options) { }
+        public DbSet<Empleado> Empleados { get; set; }
+
+        public DbSet<Alumno> Alumnos { get; set; }
+    }
+
+}
