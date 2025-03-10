d49be11ca0df76339bddb697c768039a543c680a
Fran-mndz <shiro54828@gmail.com>
2025-03-09 22:10:53 -0600

    Agregar archivos de proyecto.

---
+++ TAREA_MODULO.AppMVCCodeFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TAREA_MODULO.AppMVCCodeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ TAREA_MODULO.AppMVCDataFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TAREA_MODULO.AppMVCDataFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
